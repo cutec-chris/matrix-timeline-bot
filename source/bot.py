72fb50ba79d80f8055af95c92c8333f9b2ef7f2a
Christian U <christian@ullihome.de>
2022-04-10 14:05:20 +0200

    basic mastodon feeds works (without image embedding)

--- source/bot.py
+++ source/bot.py
@@ -1,8 +1,12 @@
 from init import *
-import functools,markdownify
+import functools,re
 servers = []
 loop = None
 lastsend = None
+async def save_servers():
+    global servers
+    with open('data.json', 'w') as f:
+        json.dump(servers,f, skipkeys=True)
 @bot.listener.on_message_event
 async def tell(room, message):
     global servers,lastsend
@@ -25,19 +29,18 @@ async def tell(room, message):
             server['clientid'] = match.args()[5]
         servers.append(server)
         loop.create_task(check_server(server))
-        with open('data.json', 'w') as f:
-            json.dump(servers,f, skipkeys=True)
+        await save_servers()
         await bot.api.send_text_message(room.room_id, 'ok')
     elif match.is_not_from_this_bot():
         for server in servers:
             if server['room'] == room.room_id:
                 pass
-async def post_entry(room,body,html_body,sender):
+async def post_html_entry(room,html_body,sender):
     await bot.api.async_client.room_send(room_id=room,
                                           message_type="m.room.message",
                                           content={
-                                              "msgtype": "m.text",
-                                              "body": body,
+                                              "msgtype": "m.text",#or m.notice seems to be shown more transparent
+                                              "body": re.sub('<[^<]+?>', '', html_body),
                                               "format": "org.matrix.custom.html",
                                               "formatted_body": sender+'<br>'+html_body})
 async def check_server(server):
@@ -45,10 +48,13 @@ async def check_server(server):
     def update_server_var():
         for server_r in servers:
             if server_r['room'] == server['room']\
-            and server_r['url'] == server['url']:
+            and server_r['feed'] == server['feed']:
                 server_r = server
     LastError = None
-    LastId = None
+    if 'LastId' in server:
+        LastId = server['LastId']
+    else:
+        LastId = None
     while True:
         try:
             if 'apikey' in server: #at time we only support mastodon with api key
@@ -62,16 +68,19 @@ async def check_server(server):
                     Mastodon = None #no mastodon instance ?
                 if Mastodon:
                     #events = await get_room_events(bot.api.async_client,server['room'])
-                    tl = Mastodon.timeline(min_id=LastId)
-                    if len(tl)>0:
-                        LastId =  tl[0]['id']
-                    for toot in tl:
-                        sender = '<img src=\"%s\"></img><a href=\"%s\">%s</a><font size="-1"> %s</font>' % (toot['account']['avatar'],toot['account']['url'],toot['account']['display_name'],toot['account']['acct'])
-                        if toot['reblog']:
-                            sender = toot['account']
-                            toot = toot['reblog']
-                        await post_entry(server['room'],toot['card']['description'],toot['content'],sender)
-                        break
+                    while True:
+                        tl = Mastodon.timeline(min_id=LastId)
+                        for toot in reversed(tl):
+                            sender = '<img src=\"%s\"></img><a href=\"%s\">%s</a><font size="-1"> %s</font>' % (toot['account']['avatar'],toot['account']['url'],toot['account']['display_name'],toot['account']['acct'])
+                            if toot['reblog']:
+                                sender = toot['account']
+                                toot = toot['reblog']
+                            await post_html_entry(server['room'],toot['content'],sender)
+                            LastId = toot['id']
+                            server['LastId'] = LastId
+                            update_server_var()
+                            await save_servers()
+                        await asyncio.sleep(60)
         except BaseException as e:
             if str(e) != LastError:
                 LastError = str(e)
@@ -98,7 +107,7 @@ async def bot_help(room, message):
                 command: follow feed_url [username] [password] [api key]
                 description: follow feed or timeline in this room
                 mastodon needs all fields filled
-                rss/atom feeds only the url (and username and apsswort when auth is needed)
+                rss/atom feeds only the url (and username and passwort when auth is needed)
             help:
                 command: help, ?, h
                 description: display help command
